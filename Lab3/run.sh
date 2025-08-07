33edaea5c5bee4d3720f9317bf54fc4cbc34af7b
Yue Zhu <SJZHZ2001@outlook.com>
2025-08-07 11:13:05 -0700

    run 5-WordLadder
    4/6/7 words are not provided

--- Lab3/run.sh
+++ Lab3/run.sh
@@ -1,5 +1,5 @@
 g++ main.cpp WordLadder.cpp -Wall -Werror -Wextra -Wuninitialized -pedantic-errors -Wconversion -o b.out && chmod +x b.out
 ./b.out words4.txt no use
-./b.out words5.txt no use
+./b.out words5.txt stays stack
 ./b.out words6.txt no use
 ./b.out words7.txt no use
