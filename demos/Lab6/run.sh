3de190700eedd7df36af88dae269f9ae5efcac4d
SJZHZ <sjzhz2001@outlook.com>
2025-08-14 11:13:27 -0700

    Lab6

--- demos/Lab6/run.sh
+++ demos/Lab6/run.sh
@@ -1,6 +1,14 @@
 # usage: bash run.sh > demo.log
 
+# for Amazon Linux 2
+# sudo yum -y remove java-11-amazon-corretto-headless golang-bin docker
+# sudo debuginfo-install $(rpm -q libgcc libstdc++)
+
+# for Amazon Linux 2023
 # sudo dnf debuginfo-install glibc-2.34-52.amzn2023.0.10.x86_64 libgcc-11.4.1-2.amzn2023.0.2.x86_64 libstdc++-11.4.1-2.amzn2023.0.2.x86_64
+
+# If you don't remember, try both commands above.
+
 g++ -W -Wall -Werror -g -std=c++14 main-hard-code.cpp 
 ./a.out
 gdb -x test_gdb.txt ./a.out
